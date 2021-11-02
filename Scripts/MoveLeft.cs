1a15db5fd056526aa27cd693ad3d287666a673d2
Quwiyu <68051831+Ajibade-Quwiyu@users.noreply.github.com>
2021-11-02 14:28:19 +0100

    Add files via upload
    this is the 2nd prototype of Create-with-code

---
+++ Scripts/MoveLeft.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30;
+    private PlayerController2 playerControllerScript;
+    private float leftBound = -15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+        if(playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
